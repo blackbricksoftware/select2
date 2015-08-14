cd4946ee29a9a5239176fefed0583dfac6220c4b
David Hayes <david@blackbricksoftware.com>
2015-08-14 15:41:02 -0500

    change width to min-width on container width

--- src/js/select2/core.js
+++ src/js/select2/core.js
@@ -110,7 +110,7 @@ define([
     var width = this._resolveWidth(this.$element, this.options.get('width'));
 
     if (width != null) {
-      $container.css('width', width);
+      $container.css('min-width', width);
     }
   };
 
